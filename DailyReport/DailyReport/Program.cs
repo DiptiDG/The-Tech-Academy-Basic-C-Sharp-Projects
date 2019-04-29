4225c0b70809271d51d14a4ac5927d71cac8a890
DiptiDG <Diptiazure1@gmail.com>
2019-04-29 16:29:46 -0700

    Adding Daily Report for student at tech academy in C# Project

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace DailyReport
+{
+    class DailyReport
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("What course are you on?");
+            string coursName = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? ");
+             bool needHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string feedBack = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedBack2 = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            byte time = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+        }
+    }
+}
