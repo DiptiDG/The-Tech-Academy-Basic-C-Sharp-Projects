55c8feced96b3b6ca59b61d81d86224f36b5926f
DiptiDG <Diptiazure1@gmail.com>
2019-04-30 12:47:01 -0700

    Adding car insurance assignment here.

---
+++ CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            byte age = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+
+            bool Qualified = (age >= 15 && DUI == false && ticket <= 3);
+            Console.WriteLine(Qualified);
+            Console.Read();
+
+        }
+    }
+}
