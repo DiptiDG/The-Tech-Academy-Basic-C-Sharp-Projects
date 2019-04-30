bdf0066a346dc393c68f09a85933dd511898233c
DiptiDG <Diptiazure1@gmail.com>
2019-04-30 14:59:00 -0700

    Adding boolean comparison assignment using while loop.

---
+++ boolcomp/boolcomp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boolcomp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter even number");
+            int num = Convert.ToInt32( Console.ReadLine());
+            int res = num % 2;
+            bool isEven = false;
+
+            while (!isEven)
+            {
+                switch (res)
+                {
+                    case 1:
+                        Console.WriteLine("You entered  odd number. Try Again!");
+                        Console.WriteLine("Enter number");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        res = num %2;
+                        break;
+                    case 0:
+                        Console.WriteLine("Wow! you entered even number");
+                        isEven = true;
+                        break;
+                    default:
+                        Console.WriteLine("Something went wrong");
+                        break;
+
+                }
+            }
+                    Console.Read();
+
+            
+        }
+    }
+}
