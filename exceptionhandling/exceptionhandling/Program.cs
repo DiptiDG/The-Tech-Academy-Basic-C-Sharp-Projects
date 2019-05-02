8651475a9db98e6f0d5886c82683f055b94acd2b
DiptiDG <Diptiazure1@gmail.com>
2019-05-02 13:24:15 -0700

    adding try and catch exception handling assignment here.

---
+++ exceptionhandling/exceptionhandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionhandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> num = new List<int>() { 22, 45, 60, 84, 40, 33 };
+            try
+            {
+
+                Console.WriteLine("Enter number to divide each number in list");
+                float num2 = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in num)
+                {
+                    float res = number / num2;
+                    Console.WriteLine(number + "divided by " + num2 + "=" + res);
+
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
