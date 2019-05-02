32fc43d39d65298124f94827bfbb405eae665f12
DiptiDG <Diptiazure1@gmail.com>
2019-05-02 16:19:55 -0700

    Adding class and Objects assignment here.

---
+++ Classnbjects/Classnbjects/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassnObjects
+{
+    class Math
+    {
+
+        //creating method that will take input from user and return its output depend on which operation we perform.
+        public int Power(int num)
+        {
+            int res = num * num;
+            return res;
+        }
+        public int Addition(int num)
+        {
+            int res = num + num;
+            return res;
+        }
+        
+        
+            public int Division(int num)
+        {
+            int res = num / 2;
+
+            return res;
+        }
+    
+        
+    }
+}
---
+++ Classnbjects/Classnbjects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassnObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int power = math.Power(number);
+           int addition = math.Addition(number);
+            int division =math.Division(number);
+            Console.WriteLine("The Given Number is " + number + "\n Power of Number is :" + power + "\n Addition" +
+                "of Number is " + addition + "\n Division of Number is: " + division);
+            Console.Read();
+
+
+        }
+    }
+}
