d53313b7890a8da251cb9faf165b205212360dd8
DiptiDG <Diptiazure1@gmail.com>
2019-05-03 14:57:15 -0700

    addingassignmeent foroptional parameter

---
+++ optionalparameter/optionalparameter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalparameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            optionalparameter optional = new optionalparameter();
+            Console.WriteLine("Enter number 1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2\t You may not need to enter this number");
+            
+            Console.WriteLine("----------------------");
+            int res = optional.calc(num1);//adding only one parameter here
+            Console.WriteLine(res);
+            int res1 = optional.calc(num1, 4);// adding two params here
+            Console.WriteLine(res1);
+            
+            Console.Read();
+        }
+    }
+}
---
+++ optionalparameter/optionalparameter/optionalparameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalparameter
+{
+    class optionalparameter
+    {
+        public int calc(int num, int time = 5)
+        {
+            for (int i = 1; i <=time; i++)
+            {
+                num = num * i;
+             
+            }
+            return num;
+        }
+    }
+}
