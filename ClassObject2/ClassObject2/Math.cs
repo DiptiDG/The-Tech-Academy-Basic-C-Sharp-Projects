f75abe85328932d60edfe5d78c56135ec10eef6d
DiptiDG <Diptiazure1@gmail.com>
2019-05-03 13:52:30 -0700

    Adding assignment here with method

---
+++ ClassObject2/ClassObject2/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject2
+{
+    class Math
+    {
+
+        public  void Power_3(int a, out int b)
+
+        {
+
+           
+
+           
+
+            b = a * a* a;
+
+            Console.WriteLine(b);
+            Console.Read();
+
+        }
+
+       
+    }
+}
