40a75eac79560edb4f49147849fa38cd246bf423
DiptiDG <Diptiazure1@gmail.com>
2019-05-03 14:25:05 -0700

    Adding assignment here.

---
+++ MoreClasses/MoreClasses/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClasses
+{
+    class Class1
+    {
+        public int Operation (int a)
+        {
+                a = a * 2;
+           
+            return a;
+         
+        }
+        public int Operation(decimal a)
+        {
+            a = a / 2;
+
+            return Convert.ToInt32(a);
+
+        }
+        public int Operation(string s)
+        {
+            int a = 2;
+          
+            return Convert.ToInt32(s) +a ;
+       
+
+        }
+    }
+}
---
+++ MoreClasses/MoreClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            // create object here for class and using method from that class by passing value.
+           int result = class1.Operation(12);
+            Console.WriteLine("The ouput for this operation is "+result);
+            int result1 = class1.Operation(10.05m);
+            Console.WriteLine("The ouput for this operation is " + result1);
+            int result2 = class1.Operation("1034");
+            Console.WriteLine("The ouput for this operation is " + result2);
+            Console.Read();
+        }
+    }
+}
