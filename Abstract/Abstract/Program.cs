686b51ff230aa484d944dc954b3333d5d21101b9
DiptiDG <Diptiazure1@gmail.com>
2019-05-06 14:36:53 -0700

    Adding abstract class here.

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();//creating object of class employee
+            emp.firstName = "Sample";
+            emp.lastName ="Student";
+            emp.sayName();//calling sayName method
+            Console.Read();
+        }
+    }
+}
