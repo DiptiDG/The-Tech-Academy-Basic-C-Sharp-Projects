b56d797e21f503c42c5c914c1cb1efc39a1d5202
DiptiDG <Diptiazure1@gmail.com>
2019-05-06 12:33:31 -0700

    adding assignment for inheritance here.

---
+++ Inheritance1/Inheritance1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employeecs employee = new Employeecs();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
