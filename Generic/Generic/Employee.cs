230362c81341845365fa0fad24c5b1567dbef3df
DiptiDG <Diptiazure1@gmail.com>
2019-05-07 14:12:39 -0700

    Adding assignment for generics parameter.

---
+++ Generic/Generic/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Employee<T>//takes genric type of parameter
+    {
+        public int Id { get; set; }
+        public List<T> things { get; set; }
+        
+    }
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate an Employee object with type "string" as its generic parameter
+            Employee<string> emp = new Employee<string>() ;
+            emp.things = new List<string>() { "Cycle", "Bus", "Toys", "Pen" };
+            for(int i =0; i<emp.things.Count; i++)
+            {
+                Console.WriteLine(emp.things[i]);
+
+            }
+
+           //Instantiate an Employee object with type "int" as its generic parameter
+            Employee<int> emp1 = new Employee<int>();
+            emp1.things = new List<int> { 1, 4, 8, 10 };
+            for (int i = 0; i < emp1.things.Count; i++)
+            {
+                Console.WriteLine(emp1.things[i]);
+
+            }
+            Console.Read();
+
+            //emp1.things.Add(4);
+            //emp1.things.Add(8);
+        }
+    }
+}
