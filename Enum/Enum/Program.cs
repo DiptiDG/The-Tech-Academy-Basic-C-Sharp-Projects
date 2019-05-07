ed197eac70479d9b01ee4d5c92d36629eaaf06bd
DiptiDG <Diptiazure1@gmail.com>
2019-05-07 15:36:17 -0700

    Adding assignment for enum here.

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumtype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                  Console.WriteLine("Enter current day of the week ");
+                string day = Console.ReadLine();
+                Daysoftheweek week =  (Daysoftheweek)Enum.Parse(typeof(Daysoftheweek), day);
+                if (week == Daysoftheweek.tuesday)
+                {
+                    Console.WriteLine("Today is tuesday");
+                }
+
+
+            }
+            catch
+            { Console.WriteLine("Please enter an actual day of the week."); }
+            Console.Read();
+        }
+
+        public enum Daysoftheweek
+        {
+            monday, tuesday, wednesday, thursday, friday, saturday, sunday
+        }
+    }
+}
