c876702ff2442d91c93e9fe325b26f5f0e19f13b
DiptiDG <Diptiazure1@gmail.com>
2019-05-08 16:39:20 -0700

    adding assignment for struct.

---
+++ structperform/structperform/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structperform
+{
+    public struct Number//creating struct Number
+
+    {
+        
+        public decimal Amount { get; set; }//Creating property amount
+    }
+}
---
+++ structperform/structperform/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structperform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 1000;
+            Console.WriteLine(num.Amount);
+            Console.Read();
+        }
+    }
+}
