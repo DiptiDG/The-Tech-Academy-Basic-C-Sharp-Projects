db4fd5054627dde1c4c35a99d80989dfa28fcd1d
DiptiDG <Diptiazure1@gmail.com>
2019-05-10 14:05:20 -0700

    updating assignment.

--- fileioperform/fileioperform/Program.cs
+++ fileioperform/fileioperform/Program.cs
@@ -11,13 +11,21 @@ namespace fileioperform
     {
         static void Main(string[] args)
         {
+            //DateTime yearofbirth = new DateTime(1995, 5, 6, 3, 23, 40);
+            //DateTime yearofgraduation = new DateTime(2013, 3, 6, 2, 30, 52);
+
+            //TimeSpan ageatgraduation = yearofgraduation - yearofbirth;
             Console.WriteLine("Enter a Number");
             int number = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(number);
+            Console.WriteLine("-----------------------------");
             using (StreamWriter num = new StreamWriter("C:\\Users\\Dipti\\number.txt", true))
             {
                 num.WriteLine(number);
             }
+            string readnum = File.ReadAllText("C:\\Users\\Dipti\\number.txt");
+            Console.WriteLine(readnum);
+            Console.Read();
         }
     }
 }
