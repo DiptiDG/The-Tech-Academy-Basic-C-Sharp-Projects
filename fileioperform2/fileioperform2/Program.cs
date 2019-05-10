eb325ee4c14bbbc3f2b5968eac237934f25edac2
DiptiDG <Diptiazure1@gmail.com>
2019-05-10 14:33:21 -0700

    adding assignment for date time.

---
+++ fileioperform2/fileioperform2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileioperform2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine(DateTime.Now);
+           Console.WriteLine("enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(number));
+           
+            Console.Read();
+            }
+            
+        }
+    }
