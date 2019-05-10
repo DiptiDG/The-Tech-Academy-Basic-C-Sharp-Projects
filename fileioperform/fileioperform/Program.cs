03f0e3c7f84a19415f0a10f517073581c132b06b
DiptiDG <Diptiazure1@gmail.com>
2019-05-10 13:13:11 -0700

    adding assignment for file I/O.

---
+++ fileioperform/fileioperform/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileioperform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+            using (StreamWriter num = new StreamWriter("C:\\Users\\Dipti\\number.txt", true))
+            {
+                num.WriteLine(number);
+            }
+        }
+    }
+}
