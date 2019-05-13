0c32f0c3934acaa8df61845ceaa9dcb285b1918d
DiptiDG <Diptiazure1@gmail.com>
2019-05-13 13:55:19 -0700

    Adding assignment for exception handling.

---
+++ MoreException/MoreException/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+          
+            try {
+                Console.WriteLine("What is your Age?");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0)
+                {
+                    throw new Exception();
+                }
+                var year = DateTime.Now.Year - age;
+                Console.WriteLine("User born in {0}", year);
+               
+              }
+
+
+            catch (FormatException)
+            {
+                Console.WriteLine("An error occured. something is wrong");//error message
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Only positive number Please.");// error messagefor -ve or 0number
+                Console.ReadLine();
+                return;
+            }
+            
+        }
+    }
+}
