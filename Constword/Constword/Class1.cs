9f4311433e1dccba3bcf118d5be93df527a42c08
DiptiDG <Diptiazure1@gmail.com>
2019-05-13 11:13:38 -0700

    Adding assignment here.

---
+++ Constword/Constword/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constword
+{
+   public  class Class1
+    {
+      //Chaining two constructor together
+        public Class1(string student) : this(60, student)
+        {
+
+        }
+        public Class1(int marks, string name)
+        {
+            Marks = marks;
+            StudentName = name;
+           
+        }
+        public int Marks { get; set; }
+        public string StudentName { get; set; }
+    }
+}
---
+++ Constword/Constword/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Subject = "Physics";//using const keyword.
+           Class1 cl = new Class1("Dipti");
+            var marksforsub = cl.Marks;//using vr keyword
+            Console.WriteLine("marks of student is {0} for subject {1}", marksforsub, Subject);
+            Console.Read();
+        }
+    }
+}
